d0eea321fb00a903bff3357cd7f6292b6c518932
Amit Banerjee <40642807+AmitBaanerjee@users.noreply.github.com>
2019-03-05 21:13:23 -0500

    New Feature Help page changes

--- ComingSoon.php
+++ ComingSoon.php
@@ -5,72 +5,36 @@ include 'util.php';
 
 my_session_start();
 ?>
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coming Soon | Choreographic Lineage</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.1/foundation.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Asap">
-    <link rel="stylesheet" href="../css/global.css">
-    <style>
-    .loader {
-      margin-top:50px;
-        border: 16px solid #f3f3f3;
-        border-radius: 50%;
-        border-top: 16px solid green;
-        border-bottom: 16px solid green;
-        width: 60px;
-        height: 60px;
-        -webkit-animation: spin 4s linear infinite alternate;
-        animation: spin 4s linear infinite alternate;
-        margin-left: auto;
-        margin-right: auto;
-    
-        }
-    html,body{
-      margin:0;
-     padding:0;
-     height:100%;
-      }
-    
-    .footer {
-      position:absolute;
-      bottom:0;
-      margin-bottom: 2%;
-      width:100%;
-      height:60px;
+<html>
+<head>
+  <title>Coming Soon Page</title>
+  <style>
+    .footer{
+      margin-top: 2.9%;
     }
-    .content {
-      height: 1000px; /* Changed this height */
-      padding-bottom:60px;
-    }
-    
-    @-webkit-keyframes spin {
-    0% { -webkit-transform: rotate(0deg); }
-    100% { -webkit-transform: rotate(360deg); }
-    }
-
-    @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-    }
-    .portrait {
-       width: 200px;
-        }
   </style>
-  </head>
-
-  <body>
-      <div id="content">
-        <h3 class="text-center" style="font-family: 'Open Sans Condensed',sans-serif ">Coming Soon!</h3>
-        <div class="loader" ></div>
-      </div>
-
-  </body>
-  <div class="footer">
+</head>
+<div class="container">
+  <div style="text-align:center">
+    <h4>Kindly Tell Us Your Query</h4>
+  </div>
+  <div class="row">
+      <form method ="post" action="helpEmail.php" name="emailform">
+        <label for="fname">Name</label>
+        <input type="text" id="name" name="name" placeholder="Your name..">
+        <label for="lname">Email</label>
+        <input type="Email" id="mail" name="mail" placeholder="Your Email-ID..">
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="content"
+         name="subject" 
+         placeholder="Please start writing here.." 
+         style="height:110px">
+        </textarea>
+        <input type="submit" name="submit" value="submit" style="color:"cyan">
+      </form>
+    </div>
+</div>
+<div class="footer">
   <?php
   include 'footer.php';
   ?>
---
+++ helpEmail.php
@@ -0,0 +1,70 @@
+<?php
+include 'path.php';
+include 'menu.php';
+include 'util.php';
+
+my_session_start();
+
+if(!isset($_POST['submit'])){
+  echo"error, you need to submit form";
+}
+$name=$_POST['name'];
+$visitor_email=$_POST['mail'];
+$content=$_POST['content'];
+
+if(empty($name)||empty($visitor_email)){
+  echo "error, Name and email are mandatory!";
+
+}else{
+include 'php/lib/PHPMailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer;
+
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'hobbes.cse.buffalo.edu';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = false;                               // Enable SMTP authentication
+    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->SMTPDebug=2;
+
+    $mail->setFrom('no-reply@buffalo.edu', 'Choreographic Lineage');
+
+    $mail->isHTML(true);
+    $mail->addReplyTo('choreographiclineage@gmail.com', 'Choreographic Lineage');
+    $mail->addCustomHeader('MIME-Version: 1.0');
+    $mail->addCustomHeader('Content-Type: text/html; charset=ISO-8859-1');
+    $mail->Subject = "Choreographic Lineage Appointment";
+
+    $message = "Thank you for your interest in Choreographic Lineage. A team member will contact you soon to arrange a lineage appointment.<br/><br/>Thank you,<br/>Choreographic Lineage Team";
+    $mail->addAddress($visitor_email);
+    $mail->Body = $content;
+    if(!$mail->send()){
+      echo"failed";
+    }
+    else{
+      echo"passed";
+    }
+// include 'php/lib/PHPMailer/PHPMailerAutoload.php';
+//     $mail = new PHPMailer;
+//     $mail->isSMTP();
+//     $mail->SMTPDebug=1;
+//     $mail->SMTPAuth=false;
+//     $mail->Host = 'hobbes.cse.buffalo.edu';//no-reply@buffalo.edu
+//     //$mail->Port=587;//587
+//     $mail->IsHTML(true);
+//     $mail->Username="rosicky210@gmail.com";
+//     $mail->Password="4Y2ywTfiXjVaAvp";
+//     $mail->setFrom("rosicky210@gmail.com");
+//     $mail->Subject="Choreographic Lineage Help Query";
+//     $mail->Body=$content;
+//     $mail->AddAddress($visitor_email);
+
+//     if(!$mail->send()){
+//       echo"mail not sent";
+//     }
+//     else{
+//       echo"mail sent";
+//     }
+  }
+include 'footer.php';
+
+?>
+
