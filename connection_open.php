c678e26c11c55778285ecb35cb04b39391565e10
Amit Banerjee <40642807+AmitBaanerjee@users.noreply.github.com>
2019-04-01 23:08:56 -0400

    Update connection_open.php

--- connection_open.php
+++ connection_open.php
@@ -1,14 +1,14 @@
 <?php
-// $mysqli_hostname = "stark.cse.buffalo.edu";
-// $mysqli_user="cl";
-// $mysqli_password="DclSQLwebsiteLineage";
-// $mysqli_database="choreographiclineage_db";
+$mysqli_hostname = "stark.cse.buffalo.edu";
+$mysqli_user="cl";
+$mysqli_password="DclSQLwebsiteLineage";
+$mysqli_database="choreographiclineage_db";
 
-$mysqli_hostname = "localhost";
-$mysqli_user="root";
+//$mysqli_hostname = "localhost";
+//$mysqli_user="root";
 # Comment this if working using XAMPP
-$mysqli_password="";
-$mysqli_database="Choreographiclineage2019";
+//$mysqli_password="";
+//$mysqli_database="Choreographiclineage2019";
 
 #Comment this if working using WAMP
 #$mysqli_password="";
