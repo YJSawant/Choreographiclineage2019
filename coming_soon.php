9e28525f724952502bd62909d93a7f923fcaf447
Amit Banerjee <40642807+AmitBaanerjee@users.noreply.github.com>
2019-04-04 23:19:22 -0400

    Pre populate email and name on coming soon.php

--- coming_soon.php
+++ coming_soon.php
@@ -4,6 +4,17 @@ include 'menu.php';
 include 'util.php';
 
 my_session_start();
+if (isset($_SESSION["user_email_address"])){
+  $user_email_address=$_SESSION["user_email_address"];
+  $user_firstname=$_SESSION["user_firstname"];
+  $user_lastname=$_SESSION["user_lastname"];
+  $user_name=$user_firstname." ".$user_lastname;
+
+}
+else{
+  $user_name="";
+  $user_email_address="";
+}
 ?>
 <html>
 <head>
@@ -21,12 +32,12 @@ my_session_start();
   <div class="row">
       <form method ="post" action="help_mediator.php" name="emailform">
         <label for="fname">Name</label>
-        <input type="text" id="name" name="name" placeholder="Your name">
+        <input type="text" id="name" name="name" placeholder="Your name" value="<?php echo $user_name?>">
         <label for="lname">Email</label>
-        <input type="Email" id="mail" name="mail" placeholder="Your Email-ID">
+        <input type="Email" id="mail" name="mail" placeholder="Your Email-ID" value="<?php echo $user_email_address?>">
         <label for="subject">Subject</label>
         <textarea id="subject" name="content"
-         placeholder="Please start writing here" 
+         placeholder="Please start writing here"
          style="height:110px">
         </textarea>
         <input type="submit" name="submit" value="Submit" style="color:black">
